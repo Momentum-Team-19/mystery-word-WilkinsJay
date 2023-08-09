c405ef8f8db5e8162ade994bdd2120a895ba6e84
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-08-09 15:41:58 -0400

    getting started

--- mystery_word.py
+++ mystery_word.py
@@ -1,3 +1,15 @@
+def select_word(filename):
+    pass
+    # take in a file and return a word in list, selected at random
+
+def display_secret_board(word):
+    # this function will display as underscores or a letter that were guessed correctly
+
+def user_guess(word):
+    # take in user input and compare it the word, will return if it was wrong or the correct letter
+
+
+
 def play_game():
     pass
 

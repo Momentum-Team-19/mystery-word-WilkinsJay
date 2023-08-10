866ac8c19e4f4ccc977ccdf454b42bc6098b506a
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-08-10 15:43:54 -0400

    working game

--- mystery_word.py
+++ mystery_word.py
@@ -19,18 +19,36 @@ def display_secret_board(word, guess_letters):
     return chosen_word
     # this function will display as underscores or a letter that were guessed correctly
 
-def user_guess(word):
-    pass
-    # while counter > 0:
-    #     if counter! = 
+def user_guess(counter):
+    while counter > 0:
+        if counter != 0:
+            print(f'You have {counter} guesses left')
+        guess = input('guess a letter: ').lower().strip()
+        if len(guess) != 1 or not guess.isalpha():
+            print('Please enter only 1 letter')
+        else:
+            return guess
     # take in user input and compare it the word, will return if it was wrong or the correct letter
 
 
 
 def play_game():
     random_word = select_word('short_list.txt')
-    print(random_word)
     guess_letters = []
+    counter = 8
+    while counter > 0:
+        display = display_secret_board(random_word, guess_letters)
+        print(display)
+        if '_' not in display:
+            print('You got it correct!', random_word)
+            break
+        guess = user_guess(counter)
+        guess_letters.append(guess)
+        if guess not in random_word:
+            counter -= 1
+    else:
+        print('Better luck next time 🤓')
+    print(random_word)
     print(display_secret_board(random_word, guess_letters))
 
 
